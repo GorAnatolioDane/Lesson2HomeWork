49715d55c05d20ebeb9ed82d031fbc14f0abc3cc
Dane <slip_dane@mail.ru>
2022-10-05 16:37:06 +0400

    AddHW1

---
+++ HomeWork1/SeckondDigit/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+void secondDigit (int userNumber)
+{
+
+if (userNumber > 99 && userNumber < 1000)
+{
+    int lastNumber = userNumber / 10;
+    int secondNumber = lastNumber % 10;
+    Console.WriteLine($"Вторая цифра числа {userNumber} равна {secondNumber}"); 
+}
+else    
+{
+    Console.WriteLine("Чилсло не трёхзначное");
+}
+}
+secondDigit(userNumber);
