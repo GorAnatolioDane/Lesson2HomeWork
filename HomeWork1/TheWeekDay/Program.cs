dc6258c2ae22e1b9427cdaec07b90e978ccae64d
Dane <slip_dane@mail.ru>
2022-10-05 17:10:20 +0400

    AddWeekDay

---
+++ HomeWork1/TheWeekDay/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckTheDayOfWeek (int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день выходной");    
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Это не день недели");
+    }
+    else 
+    {
+        Console.WriteLine("Этот день рабочий");
+    }
+    
+}
+CheckTheDayOfWeek(dayNumber);
